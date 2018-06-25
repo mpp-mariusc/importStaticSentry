e306f667f125e67df6b70e5af53ee784cca5636a
Marius Ciocanel <mariusc@mypinpad.com>
2018-06-25 12:57:53 +0100

    Initial Commit

---
+++ importStaticSentry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  importStaticSentry
+//
+//  Created by Marius Ciocanel on 25/06/2018.
+//  Copyright © 2018 Marius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
